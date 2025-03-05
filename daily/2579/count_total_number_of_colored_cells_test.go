cfdd98ff977b18611807cb981ff21fc109f80db9
Velimir <87824619+VtheSaint@users.noreply.github.com>
2025-03-05 12:20:39 +0300

    daily question #2579: count total number of colored cells

---
+++ daily/2579/count_total_number_of_colored_cells_test.go
@@ -0,0 +1,37 @@
+package _2579
+
+import "testing"
+
+func Test_coloredCells(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "case 1",
+			args: args{n: 1},
+			want: 1,
+		},
+		{
+			name: "case 2",
+			args: args{n: 2},
+			want: 5,
+		},
+		{
+			name: "case 3",
+			args: args{n: 3},
+			want: 13,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := coloredCells(tt.args.n); got != tt.want {
+				t.Errorf("coloredCells() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
